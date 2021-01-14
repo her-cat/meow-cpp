c73f50ff4432cd8959f48e7a7e93f5b7967b71ab
her-cat <i@her-cat.com>
2021-01-14 19:23:50 +0800

    Coroutine::getCid()

--- meow_coroutine_util.cc
+++ meow_coroutine_util.cc
@@ -7,13 +7,13 @@ using meow::Coroutine;
 /* 记录用户让出的协程 */
 static std::unordered_map<long, Coroutine *> user_yield_coroutines;
 
+/* 定义无参数 */
+ZEND_BEGIN_ARG_INFO_EX(arginfo_study_coroutine_void, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
 /* 定义 Coroutine::create 方法的参数 */
 ZEND_BEGIN_ARG_INFO_EX(arginfo_meow_coroutine_create, 0, 0, 1)
     ZEND_ARG_CALLABLE_INFO(0, func, 0)
-ZEND_END_ARG_INFO()
-
-/* 定义 Coroutine::yield 方法的参数 */
-ZEND_BEGIN_ARG_INFO_EX(arginfo_meow_coroutine_yield, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 /* 定义 Coroutine::resume 方法的参数 */
@@ -70,12 +70,19 @@ PHP_METHOD(meow_coroutine_util, resume)
     RETURN_TRUE
 }
 
+/* 获取当前协程的 id */
+PHP_METHOD(meow_coroutine_util, getCid)
+{
+    RETURN_LONG(Coroutine::get_current()->get_cid())
+}
+
 /* 定义 Coroutine 的方法列表 */
 const zend_function_entry meow_coroutine_util_methods[] =
 {
     PHP_ME(meow_coroutine_util, create, arginfo_meow_coroutine_create, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
-    PHP_ME(meow_coroutine_util, yield, arginfo_meow_coroutine_yield, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    PHP_ME(meow_coroutine_util, yield, arginfo_study_coroutine_void, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
     PHP_ME(meow_coroutine_util, resume, arginfo_meow_coroutine_resume, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    PHP_ME(meow_coroutine_util, getCid, arginfo_study_coroutine_void, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
     PHP_FE_END
 };
 
